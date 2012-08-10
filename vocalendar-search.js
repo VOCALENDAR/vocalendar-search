1020b64cf8533eee52626c387683f7ed84258e47
mryou <ryou@maple.dti2.ne.jp>
2012-08-10 13:23:12 +0900

    parseIntは基数を入れないと問題が発生すると聞いて

--- vocalendar-search.js
+++ vocalendar-search.js
@@ -49,12 +49,12 @@ jQuery( function($){
 			
 			var m = str.match(/^(\d{4})-(\d{2})-(\d{2})[T ](\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)(Z|([+-])(\d{2}):(\d{2}))$/);
 			if ( m ) {
-				year = parseInt(m[1]);
-				mon = parseInt(m[2]);
-				day = parseInt(m[3]);
-				hour = parseInt(m[4]);
-				min = parseInt(m[5]);
-				sec = parseInt(m[6]);
+				year = parseInt(m[1], 10);
+				mon = parseInt(m[2], 10);
+				day = parseInt(m[3], 10);
+				hour = parseInt(m[4], 10);
+				min = parseInt(m[5], 10);
+				sec = parseInt(m[6], 10);
 				if ( m[7] != 'Z' ) {
 					offset += ( m[8] == '+' ? 1 : -1 ) * (parseInt(m[9], 10) * 60 + parseInt(m[10], 10)) * 1000 * 60;
 				}
